068aebf01aa7bd1185ef780792eae82c78e684e9
victorsootho <85297432+victorsootho@users.noreply.github.com>
2022-12-02 18:25:49 +0200

    Update setupProxy.js

--- src/components/blog/setupProxy.js
+++ src/components/blog/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/posts", {
+    createProxyMiddleware("/blogs", {
       target: "https://kofiserver.onrender.com/api",
       changeOrigin: true,
     })
